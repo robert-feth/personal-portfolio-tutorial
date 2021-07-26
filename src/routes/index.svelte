111ae6b75fa153979f6560e52b281eae64b55fb2
Bob Feth <bob.feth@stsaviationgroup.com>
2021-07-26 11:00:20 -0400

    Working up to index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,27 @@
-<h1 class="text-3xl text-[#cedb00]">Testing!</h1>
+<script context="module">
+    export const prerender = true;
+
+    export async function load({ page, fetch, session, context }) {
+        let res = await fetch('/posts/recent-posts.json');
+
+        return {
+            props: {
+                posts: await res.json()
+            }
+        };
+    }
+</script>
+
+<script>
+    import Hero from '../components/Hero.svelte';
+    import BlogSnippets from '../components/BlogSnippets.svelte';
+
+    export let posts;
+</script>
+
+<Hero
+    title="Bob Feth"
+    subTitle="Stuff about me"
+/>
+
+<BlogSnippets {posts} title="Recent Posts" />
